78f47cae01c4e6036fb40dc47d9ce142f8c81235
Arvin Fabales <Arvin Fabales@ARVINFABALES>
2023-08-16 13:27:43 +0800

    Add project files.

---
+++ DentAppMk2/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿
+namespace DentAppMk2
+{
+    partial class Win1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shopName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // shopName
+            // 
+            this.shopName.AutoSize = true;
+            this.shopName.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shopName.Location = new System.Drawing.Point(12, 9);
+            this.shopName.Name = "shopName";
+            this.shopName.Size = new System.Drawing.Size(335, 46);
+            this.shopName.TabIndex = 0;
+            this.shopName.Text = "KFD Dental Clinic";
+            this.shopName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Win1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.shopName);
+            this.Name = "Win1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label shopName;
+    }
+}
+
---
+++ DentAppMk2/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DentAppMk2
+{
+    public partial class Win1 : Form
+    {
+        public Win1()
+        {
+            InitializeComponent();
+        }
+
+        }
+}
